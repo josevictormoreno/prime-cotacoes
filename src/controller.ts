ed92995cc7a6b230fcf292da33e0546a0cdd5877
josevictormoreno <josevictor.admoreno@gmail.com>
2023-05-28 16:37:12 -0300

    Inicia correção do puppeteer

--- src/controller.ts
+++ src/controller.ts
@@ -1,47 +1,26 @@
 import {NextFunction, Request, Response} from 'express'
 import puppeteer from 'puppeteer'
 
-const getDollar = async (req: Request, res: Response, next: NextFunction, page: any) => {
-  await page.goto('https://www.google.com/search?q=dolar&sxsrf=ALiCzsbHDZsdh2ptO0x3uh8iEQHuT6wqkg%3A1667865643031&ei=K5xpY4a1AYDN1sQPvdeuuAk&ved=0ahUKEwjG9r3Zo537AhWAppUCHb2rC5cQ4dUDCA8&uact=5&oq=dolar&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIJCCMQJxBGEIICMgQIIxAnMgoIABCxAxCDARBDMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwEyCwgAEIAEELEDEIMBMgoIABCxAxCDARBDMgoILhDHARDRAxBDMgQIABBDMgsIABCABBCxAxCDAToKCAAQRxDWBBCwAzoHCAAQsAMQQzoNCAAQ5AIQ1gQQsAMYAToMCC4QyAMQsAMQQxgCOggILhCxAxCDAToOCC4QgAQQsQMQgwEQ1AI6CwguEIAEEMcBENEDOgsILhCxAxCDARDUAjoGCCMQJxATOgQILhBDOg0IABCABBCxAxCDARAKSgQITRgBSgQIQRgASgQIRhgBUJ4KWNYNYPQOaAFwAXgAgAG3AYgB7AWSAQMwLjWYAQCgAQHIARLAAQHaAQYIARABGAnaAQYIAhABGAg&sclient=gws-wiz-serp');
-  const content = await page.evaluate(() => {
-    return {
-      //@ts-ignore
-      value: document.querySelector(".SwHCTb").innerHTML
-    }
-  });
-  return content.value
-}
-const getEuro = async (req: Request, res: Response, next: NextFunction, page: any) => {
-  await page.goto('https://www.google.com/search?q=euro&oq=euro&aqs=chrome.0.69i59.779j0j1&sourceid=chrome&ie=UTF-8');
-  const content = await page.evaluate(() => {
-    return {
-      //@ts-ignore
-      value: document.querySelector(".SwHCTb").innerHTML
-    }
-  });
-  return content.value
-}
-const getLibra = async (req: Request, res: Response, next: NextFunction, page: any) => {
-  await page.goto('https://www.google.com/search?q=libra+moeda&sxsrf=ALiCzsa1dRwo1Egy1S3ob5kzlIXiLXY0gw%3A1670682996797&ei=dJmUY9SZMP7P1sQPgaWu0A0&oq=libra&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgcIIxCwAxAnMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgcIABCwAxBDMgcIABCwAxBDMgcIABCwAxBDMgcIABCwAxBDMg0IABDkAhDWBBCwAxgBMg0IABDkAhDWBBCwAxgBMg0IABDkAhDWBBCwAxgBMhIILhDHARDRAxDIAxCwAxBDGAIyDwguENQCEMgDELADEEMYAkoECEEYAEoECEYYAVAAWABgiwhoAXABeACAAQCIAQCSAQCYAQDIARHAAQHaAQYIARABGAnaAQYIAhABGAg&sclient=gws-wiz-serp');
-  const content = await page.evaluate(() => {
-    return {
-      //@ts-ignore
-      value: document.querySelector(".SwHCTb").innerHTML
-    }
-  });
-  return content.value
-}
-const getCoffeTv = async (req: Request, res: Response, next: NextFunction, page: any) => {
-  await page.goto('http://www.cccv.org.br/')
+// const getCoins = async (req: Request, res: Response, next: NextFunction, page: any) => {
+//   await page.goto('https://br.investing.com/currencies/exchange-rates-table')
+//   const result = await page.evaluate(() => {
+//     const rows = document.querySelectorAll('.genTbl tr');
+//     return Array.from(rows, row => {
+//       const columns = row.querySelectorAll('td');
+//       return Array.from(columns, column => column.innerText);
+//     });
+//   });
+//   // res.status(200).json({"cafe arabica": result})
+//   return result
+// }
+const getCoffeTv = async (req: Request, res: Response, next: NextFunction) => {
+    const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/search?client=opera-gx&q=dolar&sourceid=opera&ie=UTF-8&oe=UTF-8')
   const result = await page.evaluate(() => {
-    const rows = document.querySelectorAll('.table-body li');
-    return Array.from(rows, row => {
-      const columns = row.querySelectorAll('p');
-      return Array.from(columns, column => column.innerText);
-    });
+    const dolar = document.querySelector('.DFlfde SwHCTb');
+    res.status(200).json({dolar: dolar})
   });
-  // res.status(200).json({"cafe arabica": result})
-  return result[5][0]
 }
 const getCoffeCooabriel = async (req: Request, res: Response, next: NextFunction, page: any) => {
   await page.goto('https://cooabriel.coop.br/en/')
@@ -62,26 +41,15 @@ const getDate = (req: Request, res: Response, next: NextFunction) => {
   let date = '' 
   return date = date.concat(day, '/', month, '/', year)
 }
-const getInfo = async (req: Request, res: Response, next: NextFunction) => {
-  const date = getDate(req,res,next)
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const dollar = await getDollar(req, res, next, page)
-  const euro = await getEuro(req, res, next, page)
-  const libra = await getLibra(req, res, next, page)
-  const cooabriel = await getCoffeCooabriel(req, res, next, page)
-  const tv = await getCoffeTv(req, res, next, page)
-  const message = 
-  `🌱Prime Corretora de Café🌱 Linhares
-  📈 * Cotações ${date} * 📉
-  Dolar 🇺🇸 ${dollar}
-  Libra 🏴󠁧󠁢󠁥󠁮󠁧󠁿 ${libra}
-  Euro 🇪🇺 ${euro}
+// const getInfo = async (req: Request, res: Response, next: NextFunction) => {
+//   const date = getDate(req,res,next)
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   // const coins = await getCoins(req, res, next, page)
+//   const cooabriel = await getCoffeCooabriel(req, res, next, page)
+//   const tv = await getCoffeTv(req, res, next, page)
+//   const message = cooabriel
+//   res.status(200).json({message: message})
+// }
 
-  📺 *TV CCCV* 📺 
-  ${tv}
-  `
-  res.status(200).json({message: message})
-}
-
-export default {getInfo, getDate}
+export default { getCoffeTv}
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import controller from './controller'
 
 const router = express.Router()
-router.get('/info', controller.getInfo)
-router.get('/test', controller.getDate)
+// router.get('/info', controller.getInfo)
+router.get('/test', controller.getCoffeTv)
 
 export = router
